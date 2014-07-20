a01200d6a37d8699cf238ff9bc23fb8ec0bb4c6f
chatgris <julien@stormz.me>
2014-07-20 15:42:53 +0200

    Ident
    Signed-off-by: chatgris <julien@stormz.me>

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,33 +14,33 @@ module.exports = function (grunt) {
 
 
     grunt.initConfig({
-      jshint: {
-          options: {
-            jshintrc: '.jshintrc'
-          },
-          all: [
-              'Gruntfile.js',
-              'app/js/**/*.js',
-              '!app/js/vendor/**/*.js'
-          ]
-      },
-      sass: {
-          options: {
-              cacheLocation: '.tmp/.sass-cache'
-          },
-          dev: {
-              options: {
-                  style: 'expanded',
-                  lineComments: true
-              },
-              files: sassFiles
-          },
-          prod: {
-              options: {
-                  style: 'compressed'
-              },
-              files: sassFiles
-          }
-      }
+        jshint: {
+            options: {
+              jshintrc: '.jshintrc'
+            },
+            all: [
+                'Gruntfile.js',
+                'app/js/**/*.js',
+                '!app/js/vendor/**/*.js'
+            ]
+        },
+        sass: {
+            options: {
+                cacheLocation: '.tmp/.sass-cache'
+            },
+            dev: {
+                options: {
+                    style: 'expanded',
+                    lineComments: true
+                },
+                files: sassFiles
+            },
+            prod: {
+                options: {
+                    style: 'compressed'
+                },
+                files: sassFiles
+            }
+        }
     });
 };
